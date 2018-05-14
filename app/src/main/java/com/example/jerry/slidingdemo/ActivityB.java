2570146d5fc662aa21e501d28d1d38e32fdb6afa
jerry-wang0 <gl1983_@126.com>
2018-05-14 21:53:52 +0800

    clean

--- app/src/main/java/com/example/jerry/slidingdemo/ActivityB.java
+++ app/src/main/java/com/example/jerry/slidingdemo/ActivityB.java
@@ -42,55 +42,36 @@ public class ActivityB extends AppCompatActivity implements OnChartGestureListen
 
         // no description text
         mChart.getDescription().setEnabled(false);
-
         // enable touch gestures
         mChart.setTouchEnabled(true);
-
         // enable scaling and dragging
         mChart.setDragEnabled(true);
         mChart.setScaleEnabled(true);
-        // mChart.setScaleXEnabled(true);
-        // mChart.setScaleYEnabled(true);
-
         // if disabled, scaling can be done on x- and y-axis separately
         mChart.setPinchZoom(true);
-
-        // set an alternative background color
-        // mChart.setBackgroundColor(Color.GRAY);
-
         // create a custom MarkerView (extend MarkerView) and specify the layout
         // to use for it
         MyMarkerView mv = new MyMarkerView(this, R.layout.custom_marker_view);
         mv.setChartView(mChart); // For bounds control
         mChart.setMarker(mv); // Set the marker to the chart
-
         // x-axis limit line
         LimitLine llXAxis = new LimitLine(10f, "Index 10");
         llXAxis.setLineWidth(4f);
         llXAxis.enableDashedLine(10f, 10f, 0f);
         llXAxis.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
         llXAxis.setTextSize(10f);
-
         XAxis xAxis = mChart.getXAxis();
         xAxis.enableGridDashedLine(10f, 10f, 0f);
-        //xAxis.setValueFormatter(new MyCustomXAxisValueFormatter());
-        //xAxis.addLimitLine(llXAxis); // add x-axis limit line
-
-//        Typeface tf = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
         LimitLine ll1 = new LimitLine(150f, "Upper Limit");
         ll1.setLineWidth(4f);
         ll1.enableDashedLine(10f, 10f, 0f);
         ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
         ll1.setTextSize(10f);
-        //ll1.setTypeface(tf);
-
         LimitLine ll2 = new LimitLine(-30f, "Lower Limit");
         ll2.setLineWidth(4f);
         ll2.enableDashedLine(10f, 10f, 0f);
         ll2.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
         ll2.setTextSize(10f);
-        //ll2.setTypeface(tf);
-
         YAxis leftAxis = mChart.getAxisLeft();
         leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
         leftAxis.addLimitLine(ll1);
@@ -100,33 +81,14 @@ public class ActivityB extends AppCompatActivity implements OnChartGestureListen
         //leftAxis.setYOffset(20f);
         leftAxis.enableGridDashedLine(10f, 10f, 0f);
         leftAxis.setDrawZeroLine(false);
-
-        // limit lines are drawn behind data (and not on top)
         leftAxis.setDrawLimitLinesBehindData(true);
-
         mChart.getAxisRight().setEnabled(false);
-
-        //mChart.getViewPortHandler().setMaximumScaleY(2f);
-        //mChart.getViewPortHandler().setMaximumScaleX(2f);
-
         // add data
         setData(45, 100);
-
-//        mChart.setVisibleXRange(20);
-//        mChart.setVisibleYRange(20f, AxisDependency.LEFT);
-//        mChart.centerViewTo(20, 50, AxisDependency.LEFT);
-
         mChart.animateX(2500);
-        //mChart.invalidate();
-
-        // get the legend (only possible after setting data)
         Legend l = mChart.getLegend();
-
         // modify the legend ...
         l.setForm(Legend.LegendForm.LINE);
-
-        // // dont forget to refresh the drawing
-        // mChart.invalidate();
     }
 
     @Override
@@ -136,13 +98,11 @@ public class ActivityB extends AppCompatActivity implements OnChartGestureListen
 
 
     private void setData(int count, float range) {
-
         ArrayList<Entry> values = new ArrayList<Entry>();
         for (int i = 0; i < count; i++) {
             float val = (float) (Math.random() * range) + 3;
             values.add(new Entry(i, val, getResources().getDrawable(R.drawable.star)));
         }
-
         LineDataSet set1;
         if (mChart.getData() != null &&
                 mChart.getData().getDataSetCount() > 0) {
@@ -175,7 +135,6 @@ public class ActivityB extends AppCompatActivity implements OnChartGestureListen
             dataSets.add(set1); // add the datasets
             // create a data object with the datasets
             LineData data = new LineData(dataSets);
-
             // set data
             mChart.setData(data);
         }
